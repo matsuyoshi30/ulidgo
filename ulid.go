86c5f0889fa7ed244a6e6a96761e3aa32643cc0d
matsuyoshi30 <sfbgwm30@gmail.com>
2022-04-09 12:16:35 +0900

    Fix

--- ulid.go
+++ ulid.go
@@ -63,40 +63,19 @@ func (u *ULID) String() string {
 	for _, b := range u[:6] {
 		fmt.Fprintf(&ts, "%08b", b)
 	}
-	// TODO: improve
 	fmt.Fprint(&dst, mustCB32(ts.String()[0:3]))
-	fmt.Fprint(&dst, mustCB32(ts.String()[3:8]))
-	fmt.Fprint(&dst, mustCB32(ts.String()[8:13]))
-	fmt.Fprint(&dst, mustCB32(ts.String()[13:18]))
-	fmt.Fprint(&dst, mustCB32(ts.String()[18:23]))
-	fmt.Fprint(&dst, mustCB32(ts.String()[23:28]))
-	fmt.Fprint(&dst, mustCB32(ts.String()[28:33]))
-	fmt.Fprint(&dst, mustCB32(ts.String()[33:38]))
-	fmt.Fprint(&dst, mustCB32(ts.String()[38:43]))
-	fmt.Fprint(&dst, mustCB32(ts.String()[43:48]))
+	for i := 3; i < 48; i += 5 {
+		fmt.Fprint(&dst, mustCB32(ts.String()[i:i+5]))
+	}
 
 	// encode random field
 	var r strings.Builder
 	for _, b := range u[6:] {
 		fmt.Fprintf(&r, "%08b", b)
 	}
-	// TODO: improve
-	fmt.Fprint(&dst, mustCB32(r.String()[0:5]))
-	fmt.Fprint(&dst, mustCB32(r.String()[5:10]))
-	fmt.Fprint(&dst, mustCB32(r.String()[10:15]))
-	fmt.Fprint(&dst, mustCB32(r.String()[15:20]))
-	fmt.Fprint(&dst, mustCB32(r.String()[20:25]))
-	fmt.Fprint(&dst, mustCB32(r.String()[25:30]))
-	fmt.Fprint(&dst, mustCB32(r.String()[30:35]))
-	fmt.Fprint(&dst, mustCB32(r.String()[35:40]))
-	fmt.Fprint(&dst, mustCB32(r.String()[40:45]))
-	fmt.Fprint(&dst, mustCB32(r.String()[45:50]))
-	fmt.Fprint(&dst, mustCB32(r.String()[50:55]))
-	fmt.Fprint(&dst, mustCB32(r.String()[55:60]))
-	fmt.Fprint(&dst, mustCB32(r.String()[60:65]))
-	fmt.Fprint(&dst, mustCB32(r.String()[65:70]))
-	fmt.Fprint(&dst, mustCB32(r.String()[70:75]))
-	fmt.Fprint(&dst, mustCB32(r.String()[75:80]))
+	for i := 0; i < 80; i += 5 {
+		fmt.Fprint(&dst, mustCB32(r.String()[i:i+5]))
+	}
 
 	return dst.String()
 }
